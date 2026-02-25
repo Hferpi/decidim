2badb967dd12c4e8ceaf95a14b236b214fe8e87c
Hector FerPi <161331335+Hferpi@users.noreply.github.com>
2026-02-25 21:26:02 +0100

    instalar nest vue tailwind y ponerlo todo en funcionamiento

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-black text-white">
+    <h1 class="text-4xl font-bold">
+      Tailwind funciona 🚀
+    </h1>
+  </div>
+</template>
